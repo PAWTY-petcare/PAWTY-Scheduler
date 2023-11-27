f38caf02837cffdee357842ab6e235a78e495f7f
XIELEVENXVISIXTEEN <126922347+XIELEVENXVISIXTEEN@users.noreply.github.com>
2023-11-27 21:36:16 +0000

    Nav pages frontend finished

--- src/js/views/schedule.js
+++ src/js/views/schedule.js
@@ -1,16 +1,20 @@
 import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
 import { useNavigate } from "react-router-dom";
+// import "../../styles/register.css"
+
 
 
 export const Schedule = () => {
+    const { store, actions } = useContext(Context);
 
+  return(
   
-<div>
-  <div className="calendar">
-  <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23039BE5&ctz=America%2FNew_York&title=Bambi%20Lee%20Cleaning%20%26%20Home%20Care%20Availability&src=MzRhZGJjNDViNjZhNzJkNDA5NzE2NjEzMzJjNDdmMTllNWY2YjBmOGU2NWEzYjJkZDllZGY5OGIwMzNhNDYzYkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%233F51B5&color=%230B8043" style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+  <div>
+    <div className="container">
+     <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FNew_York&src=YmFtYmlsZWVjaGNAZ21haWwuY29t&src=MzRhZGJjNDViNjZhNzJkNDA5NzE2NjEzMzJjNDdmMTllNWY2YjBmOGU2NWEzYjJkZDllZGY5OGIwMzNhNDYzYkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23039BE5&color=%233F51B5&color=%2333B679&color=%230B8043"></iframe>
+    </div>
   </div>
-</div>
 
 
   
@@ -18,5 +22,5 @@ export const Schedule = () => {
 
   
 
-
-}
+  );
+};
