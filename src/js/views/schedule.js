e1ae27b5ec938a89150a42be22782333c216f802
XIELEVENXVISIXTEEN <126922347+XIELEVENXVISIXTEEN@users.noreply.github.com>
2023-11-28 20:30:18 +0000

    calendar update

--- src/js/views/schedule.js
+++ src/js/views/schedule.js
@@ -12,7 +12,7 @@ export const Schedule = () => {
   
   <div>
     <div className="container">
-     <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FNew_York&src=YmFtYmlsZWVjaGNAZ21haWwuY29t&src=MzRhZGJjNDViNjZhNzJkNDA5NzE2NjEzMzJjNDdmMTllNWY2YjBmOGU2NWEzYjJkZDllZGY5OGIwMzNhNDYzYkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23039BE5&color=%233F51B5&color=%2333B679&color=%230B8043"></iframe>
+    <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ebe2e2&ctz=America%2FNew_York&showCalendars=0&showTabs=0&showPrint=0&title=Bambi%20Lee%20Cleaning%20%26%20Home%20Care%20Booking%20Availability&src=MzRhZGJjNDViNjZhNzJkNDA5NzE2NjEzMzJjNDdmMTllNWY2YjBmOGU2NWEzYjJkZDllZGY5OGIwMzNhNDYzYkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%233F51B5&color=%230B8043"></iframe>
     </div>
   </div>
 
