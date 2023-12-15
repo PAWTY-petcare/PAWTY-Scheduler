f9d5618d4346ccd94819da237426f8e37c8edbb2
XIELEVENXVISIXTEEN <126922347+XIELEVENXVISIXTEEN@users.noreply.github.com>
2023-12-15 08:46:47 +0000

    home page bubble theme add

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -4,7 +4,10 @@ import "../../styles/home.css";
 
 export const Home = () => (
 	<section className="bambi-home">
-      <img src={BPrint} alt="bambiprint" />
+      <img className="top-left" src={BPrint} alt="bambiprint" />
+	  <img className="bottom-right" src={BPrint} alt="bambiprint" />
+	  <img className="top-right" src={BPrint} alt="bambiprint" />
+	  <img className="bottom-left" src={BPrint} alt="bambiprint" />
 		<div className="content">
 			
 		</div>
