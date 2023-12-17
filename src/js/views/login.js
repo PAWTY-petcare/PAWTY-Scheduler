69d90828922409ded78d8e11a39b070527d62610
XIELEVENXVISIXTEEN <126922347+XIELEVENXVISIXTEEN@users.noreply.github.com>
2023-12-17 18:53:05 +0000

    contact page init

--- src/js/views/login.js
+++ src/js/views/login.js
@@ -1,58 +1,14 @@
 import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
 import { useNavigate } from "react-router-dom";
-// import "../../styles/register.css"
+import "../../styles/login.css";
+import IMG_1229 from "/workspaces/Bambi-Lee-CHC/src/image/IMG_1229.jpg";
 
 
 
 export const Login = () => {
-    const { store, actions } = useContext(Context);
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const navigate= useNavigate();
-    const handleSubmit= async (event)=>{
-        event.preventDefault();
-        try{
-            await actions.login(email, password);
-            setEmail("");
-            setPassword("");
-            navigate("/home")
-        }
-        catch(error){
-            console.log(error)
-        }
-    }
-    return (
-        <form className="form" onSubmit={handleSubmit}>
-        <div>
-            <div className="login-form">
-                <div className="forms">
-                <label>Email Address</label>
-                <input
-                type="email"
-                value={email}
-                placeholder="Enter your email"
-                onChange={e => setEmail(e.target.value)}
-                />
-                </div>
 
-                <div className="forms">
-                <label>Password</label>
-                <input
-                type="password"
-                value={password}
-                placeholder="Enter your password"
-                onChange={e => setPassword(e.target.value)}
-                />
-                </div>
-				
-                
-            </div>
-            <div>
-                <button type="submit">Submit</button>
-            </div>
-        </div>
-        </form>
-        // create button to send data to endpoint
-    );
-};
+    <div classname="Business-Card">
+       <img className="Bcontact" src={IMG_1229} alt="bleecontact" />
+    </div>
+}
