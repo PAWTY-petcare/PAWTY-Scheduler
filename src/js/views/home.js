2a749ee11d58d8e5d1def220bece4d4a1fbff91c
XIELEVENXVISIXTEEN <126922347+XIELEVENXVISIXTEEN@users.noreply.github.com>
2023-12-17 08:12:55 +0000

    css properties final

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,27 +3,21 @@ import BPrint from "/workspaces/Bambi-Lee-CHC/src/image/BPrint.png";
 import "../../styles/home.css";
 
 export const Home = () => (
-	<section className="bambi-home">
-		
-	  
+ <div className="container">
+	<section className="bambi-home"> <h1>Bambi-Lee<br />
+	                                 Cleaning &<br />
+									 Home<br />
+									 Care<br />
+									 </h1>
 	  <img className="bottom-left-a" src={BPrint} alt="bambiprint" />
       <img className="top-left" src={BPrint} alt="bambiprint" />
 	  <img className="top-left-a" src={BPrint} alt="bambiprint" />
 	  <img className="bottom-right" src={BPrint} alt="bambiprint" />
 	  <img className="top-right" src={BPrint} alt="bambiprint" />
 	  <img className="bottom-right-a" src={BPrint} alt="bambiprint" />
-	  <h1>BAMBI-LEE CLEANING & HOME CARE</h1>
 	  <img className="bottom-left" src={BPrint} alt="bambiprint" />
 	  <img className="top-right-a" src={BPrint} alt="bambiprint" />
-	     
-	  
-	  
-	  
-	  
-	  
-		<div className="content">
-			
-		</div>
 	</section>
+ </div>
   
 );
