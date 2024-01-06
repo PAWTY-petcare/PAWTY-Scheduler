39b856f0a0c676e5f1f8d6d4cc36dd8adf3d901e
XIELEVENXVISIXTEEN <126922347+XIELEVENXVISIXTEEN@users.noreply.github.com>
2024-01-06 18:51:39 +0000

    video disp

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -2,27 +2,29 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../../styles/navbar.css";
 import BLogo from "../../image/BLogo.png";
-import Video from "../../image/Video.mp4";
+// import BLogovid from "../../image/BLogovid.mp4";
+import { PolymerElement,html} from "@polymer/polymer";
+import "@thuoe/mp4-video-player";
 
 export const Navbar = () => {
 	
 	return (
 
   <nav className="nav-container">
-    <img className="BleeLogovid" src={Video} alt="bleelogovid" />
+      <img className="BleeLogo" src={BLogo} alt="bleelogo"></img>
       <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
+        <span className="menu-tog">MENU</span>
       </button>
     <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
       <div className="offcanvas-header">
-        <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+        <h5 className="offcanvas-title" id="offcanvasNavbarLabel"></h5>
         <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
       </div>
      <ul className="nav nav-pills nav-flush flex-column mb-auto text-center">
       <li>
         <a href="#" className="nav-link py-3 border-bottom rounded-0" aria-current="page" data-bs-toggle="tooltip" data-bs-placement="right" aria-label="Home">
 	         <Link to="/">
-	         <button className="btn btn-primary d-inline-flex align-items-center" type="button">
+	         <button className="btn btn-tertiary d-inline-flex align-items-center" type="button">
             Home
            </button>
 	         </Link>
@@ -31,7 +33,7 @@ export const Navbar = () => {
       <li>
 	      <a href="#" className="nav-link py-3 border-bottom rounded-0" data-bs-toggle="tooltip" data-bs-placement="right" aria-label="Dashboard" data-bs-original-title="Dashboard">
 	       <Link to="/login">
-	         <button className="btn btn-primary d-inline-flex align-items-center" type="button">
+	         <button className="btn btn-tertiary d-inline-flex align-items-center" type="button">
             Contact Us
            </button>
          </Link>
@@ -39,7 +41,7 @@ export const Navbar = () => {
       </li>
       <li>
 	      <Link to="/schedule">
-	       <button className="btn btn-primary d-inline-flex align-items-center" type="button">
+	       <button className="btn btn-tertiary d-inline-flex align-items-center" type="button">
          Booking Request
          </button>
 	      </Link>
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,12 +3,30 @@ import { Context } from "../store/appContext";
 import { useNavigate } from "react-router-dom";
 import BPrint from "../../image/BPrint.png";
 import "../../styles/home.css";
+import videojs from "video.js";
+import "video.js/dist/video-js.css";
+import { videoJS } from "../store/video.js";
+import Bl from "../../video/BLogovid.webm"
 
 export const Home = () => {
 
 	return(
  <div className="container">
-	<section className="bambi-home"> <h1>Bambi-Lee<br />
+	
+	
+	
+	<section className="bambi-home"> 
+	<video width="500rem" height="500rem"
+	autoplay muted controls
+	data-setup="{}"
+	title="BambiLeeLogoVid">
+		<source src={BLogovid} type="video/mpeg" />
+		
+    </video>
+	
+	
+	
+	<h1>Bambi-Lee<br />
 	                                 Cleaning<br />
 									 &<br />
 									 Home Care<br />
@@ -17,7 +35,7 @@ export const Home = () => {
 	<li>For contract Commercial property routined cleaning and maintenence</li>
 	<li>Airbnb/Short term rental property routined cleaning and maintenence</li>
 	<li>Residential property cleaning and maintenence </li>
-	<li>Organizational planning and implimentation</li>
+	<li>Organizational planning and implimentation</li> */}
 		
 	  <img className="bottom-left-a" src={BPrint} alt="bambiprint" />
       <img className="top-left" src={BPrint} alt="bambiprint" />
@@ -27,12 +45,12 @@ export const Home = () => {
 	  <img className="bottom-right-a" src={BPrint} alt="bambiprint" />
 	  <img className="bottom-left" src={BPrint} alt="bambiprint" />
 	  <img className="top-right-a" src={BPrint} alt="bambiprint" />
-	  <img className="bottom-left-b" src={BPrint} alt="bambiprint" />
+	  {/* <img className="bottom-left-b" src={BPrint} alt="bambiprint" />
 	  <img className="top-left-b" src={BPrint} alt="bambiprint" />
 	  <img className="bottom-right-b" src={BPrint} alt="bambiprint" />
-	  <img className="top-right-b" src={BPrint} alt="bambiprint" />
+	  <img className="top-right-b" src={BPrint} alt="bambiprint" /> */}
 
-	</section>
+	 </section> 
  </div>
 	);
 };
