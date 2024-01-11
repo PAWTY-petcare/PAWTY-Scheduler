f57457701d772ece1fc479b54f40b41e9c468b15
XIELEVENXVISIXTEEN <126922347+XIELEVENXVISIXTEEN@users.noreply.github.com>
2024-01-11 22:27:36 +0000

    fix

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../../styles/navbar.css";
+import BLogo from "../../image/BLogo.png";
 
 export const Navbar = () => {
 	
