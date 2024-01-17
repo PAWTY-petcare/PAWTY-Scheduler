89a5a9ae3820dc82cbab51026252d99b32de08f7
XIELEVENXVISIXTEEN <126922347+XIELEVENXVISIXTEEN@users.noreply.github.com>
2024-01-17 14:21:55 +0000

    updates

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,43 +1,55 @@
 
-import React, { useContext, useState } from "react";
+import React, { Component, useContext, useState } from "react";
 import { Context } from "../store/appContext";
 import { useNavigate } from "react-router-dom";
 import BPrint from "../../image/BPrint.png"
-
 import "../../styles/home.css";
-import videojs from "video.js";
-import "video.js/dist/video-js.css";
-import { videoJS } from "../store/video.js";
+
 
 
 export const Home = () => {
 
 	return(
  <div className="container">
-	
-	
-	
-	<section className="bambi-home"> 
-<video width="500rem" height="500rem"
-	autoplay muted controls
-	title="BambiLeeLogoVid">
-	
-		 </video> 
- 
-	
-	
-	
-	<h1>Bambi-Lee<br />
-	                                 Cleaning<br />
-									 &<br />
-									 Home Care<br />
-									 </h1>
+	<div className="App">
+	<div id="carouselExampleIndicators" className="carousel slide">
+  <div className="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <h1>Bambi-Lee<br />
+	     Cleaning<br />
+		    &<br />
+		Home Care<br /></h1>
+    </div>
+    <div className="carousel-item">
 	<h2>Scope of Service</h2>
 	<li>For contract Commercial property routined cleaning and maintenence</li>
 	<li>Airbnb/Short term rental property routined cleaning and maintenence</li>
 	<li>Residential property cleaning and maintenence </li>
-	<li>Organizational planning and implimentation</li> */}
-		
+	<li>Organizational planning and implimentation</li>
+    </div>
+  </div>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Next</span>
+  </button>
+</div>
+	<section className="bambi-home"> 
+
+	
+	
+	
+	
+	
+	 
 	  <img className="bottom-left-a" src={BPrint} alt="bambiprint" />
       <img className="top-left" src={BPrint} alt="bambiprint" />
 	  <img className="top-left-a" src={BPrint} alt="bambiprint" />
@@ -53,5 +65,7 @@ export const Home = () => {
 
 	 </section> 
  </div>
+ </div>
+		
 	);
 };
