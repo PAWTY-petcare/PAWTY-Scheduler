c9200b93d6d058220f9078c537ea4cb972337d19
XIELEVENXVISIXTEEN <126922347+XIELEVENXVISIXTEEN@users.noreply.github.com>
2024-05-06 14:54:46 +0000

    center images

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -26,47 +26,54 @@ export const Home = () => {
 	 <br />
 	 <div className="contactBut d-inline justify-content-sm-center">
 	
-	 			<Link to="/schedule"><button className="btn-dark btn-lg"><i class="fa-solid fa-calendar-days"></i></button></Link>   <button className="btn-success btn-lg" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="bottom" data-bs-content="Bottom popover"><i className="fa-solid fa-phone">Test</i> </button>   <button className="btn-primary btn-lg"> <i class="fa-solid fa-envelope"></i></button>
+	 			<Link to="/schedule"><button className="btn-dark btn-lg"><i class="fa-solid fa-calendar-days"></i></button></Link>   
+        <button className="btn-success btn-lg"><i className="fa-solid fa-phone" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="bottom" data-bs-content="Bottom popover"></i> </button>   
+        <button className="btn-primary btn-lg"> <i class="fa-solid fa-envelope"></i></button>
 </div>
 <div className="scope-of-service">
 	<img className="Banner" src={banner} alt="banner" />
-	<div class="card-container">
-  <div class="col-sm-3 mb-3 mb-sm-0">
-    <div class="card">
+
+
+	<div className="card-container">
+<div className="row row-cols-md-1 row-cols-md-2">
+  <div className="col">
+    <div className="card">
 	<img src={Commercial} class="card-img-top" alt="comm" />
-      <div class="card-body">
-        <h5 class="card-title">Commercial Property Cleaning and Maintenence</h5>
-        <a href="#" class="btn btn-primary">Book your service!</a>
+      <div className="card-body">
+        <h7 className="card-title">Commercial Property Cleaning and Maintenence</h7>
+        <Link to="/schedule"><a href="#" className="card-btn btn btn-primary btn-sm">Book your service!</a></Link>
       </div>
     </div>
   </div>
-  <div class="col-sm-3">
-    <div class="card">
-	<img src={ShortProp} class="card-img-top" alt="short" />
-      <div class="card-body">
-        <h5 class="card-title">Airbnb & Short-term Rental Cleaning and Maintenence</h5>
-        <a href="#" class="btn btn-primary">Convienient online scheduling!</a>
+  <div className="col-sm-3">
+    <div className="card">
+	<img src={ShortProp} className="card-img-top" alt="short" />
+      <div className="card-body">
+        <h7 className="card-title">Airbnb & Short-term Rental Cleaning and Maintenence</h7>
+        <Link to="/schedule"><a href="#" className="card-btn btn btn-primary btn-sm">Convienient scheduling!</a></Link>
       </div>
     </div>
   </div>
-  <div class="col-sm-3">
-    <div class="card">
-	<img src={Residential} class="card-img-top" alt="res" />
-      <div class="card-body">
-        <h5 class="card-title">Residential Property Cleaning and Maintenence </h5>
-        <a href="#" class="btn btn-primary">Customize your cleaning!</a>
+  <div className="col-sm-3">
+    <div className="card">
+	<img src={Residential} classNae="card-img-top" alt="res" />
+      <div className="card-body">
+        <h7 className="card-title">Residential Property Cleaning and Maintenence </h7>
+
+        <Link to="/schedule"><a href="#" className="card-btn btn btn-primary btn-sm">Customize your cleaning!</a></Link>
       </div>
     </div>
   </div>
-  <div class="col-sm-3">
-    <div class="card">
-	<img src={Organized} class="card-img-top" alt="org" />
-      <div class="card-body">
-        <h5 class="card-title">Organizational Planning and Implimentation</h5>
-        <a href="#" class="btn btn-primary">Get organized now!</a>
+  <div className="col-sm-3">
+    <div className="card">
+	<img src={Organized} className="card-img-top" alt="org" />
+      <div className="card-body">
+        <h7 className="card-title">Organizational Planning and Implimentation</h7>
+        <Link to="/schedule"><a href="#" className="card-btn btn btn-primary btn-sm">Get organized now!</a></Link>
       </div>
     </div>
   </div>
+</div>
 </div>
 		
 
